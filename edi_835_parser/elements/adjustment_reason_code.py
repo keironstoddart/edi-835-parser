ead0fa745e40bfda7e59cc9b93b83fd3f2b13efa
Keiron Stoddart <keiron.stoddart@gmail.com>
2021-03-02 10:36:58 -0500

    added adjustment reason codes

--- edi_835_parser/elements/adjustment_reason_code.py
+++ edi_835_parser/elements/adjustment_reason_code.py
@@ -7,7 +7,12 @@ reason_codes = {
 	'29': 'The time limit for filing has expired.',
 	'251': 'The attachment/other documentation that was received was incomplete or deficient.',
 	'2': 'Coinsurnace Amount.',
-	'96': 'Non-covered charge(s).'
+	'96': 'Non-covered charge(s). See remark code.',
+	'3': 'Co-payment Amount.',
+	'16': 'Claim/service lacks information or has submission/billing error(s).',
+	'B15':'This service/procedure requires that a qualifying service/procedure be received and covered. The qualifying other service/procedure has not been received/adjudicated.',
+	'A1':'Claim/Service denied. See remark code.',
+
 }
 
 
