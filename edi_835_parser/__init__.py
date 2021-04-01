11ff9cb95eb5902e55ff411eaa15d7c6be66a639
Keiron Stoddart <keiron.stoddart@gmail.com>
2021-04-01 19:21:09 -0400

    tests added and passed

--- edi_835_parser/__init__.py
+++ edi_835_parser/__init__.py
@@ -36,4 +36,6 @@ def _find_edi_835_files(path: str) -> List[str]:
 	return files
 
 if __name__ == '__main__':
-	pass
+	data = parse('~/Desktop/Waystar EoBs')
+	print(data.sum_payments())
+	data.to_dataframe().to_csv('~/Desktop/temp.csv')
--- tests/test_edi_835_parser.py
+++ tests/test_edi_835_parser.py
@@ -24,6 +24,26 @@ def test_to_dataframe(
 		emedny_sample,
 		all_samples,
 ):
-	blue_cross_nc_sample.to_dataframe().to_csv(f'{current_path}/output/blue_cross_nc_sample.csv')
-	emedny_sample.to_dataframe().to_csv(f'{current_path}/output/emedny_sample.csv')
-	all_samples.to_dataframe().to_csv(f'{current_path}/output/all_samples.csv')
+	payment = blue_cross_nc_sample.sum_payments()
+	blue_cross_nc_data = blue_cross_nc_sample.to_dataframe()
+
+	payment_mask = blue_cross_nc_data['type'] == 'payment'
+	assert payment == blue_cross_nc_data[payment_mask]['amount'].sum()
+
+	blue_cross_nc_data.to_csv(f'{current_path}/output/blue_cross_nc_sample.csv')
+
+	payment = emedny_sample.sum_payments()
+	emedny_data = emedny_sample.to_dataframe()
+
+	payment_mask = emedny_data['type'] == 'payment'
+	assert payment == emedny_data[payment_mask]['amount'].sum()
+
+	emedny_data.to_csv(f'{current_path}/output/emedny_sample.csv')
+
+	payment = all_samples.sum_payments()
+	all_data = all_samples.to_dataframe()
+
+	payment_mask = all_data['type'] == 'payment'
+	assert payment == all_data[payment_mask]['amount'].sum()
+
+	all_data.to_csv(f'{current_path}/output/all_samples.csv')
