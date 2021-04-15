e5607471beb00e84aa57a6e35540fa30f2bea5e4
Keiron Stoddart <keiron.stoddart@gmail.com>
2021-04-15 13:59:21 -0400

    updated tests

--- tests/test_edi_835_parser.py
+++ tests/test_edi_835_parser.py
@@ -27,23 +27,20 @@ def test_to_dataframe(
 	payment = blue_cross_nc_sample.sum_payments()
 	blue_cross_nc_data = blue_cross_nc_sample.to_dataframe()
 
-	payment_mask = blue_cross_nc_data['type'] == 'payment'
-	assert payment == blue_cross_nc_data[payment_mask]['amount'].sum()
+	assert payment == blue_cross_nc_data['paid_amount'].sum()
 
 	blue_cross_nc_data.to_csv(f'{current_path}/output/blue_cross_nc_sample.csv')
 
 	payment = emedny_sample.sum_payments()
 	emedny_data = emedny_sample.to_dataframe()
 
-	payment_mask = emedny_data['type'] == 'payment'
-	assert payment == emedny_data[payment_mask]['amount'].sum()
+	assert payment == emedny_data['paid_amount'].sum()
 
 	emedny_data.to_csv(f'{current_path}/output/emedny_sample.csv')
 
 	payment = all_samples.sum_payments()
 	all_data = all_samples.to_dataframe()
 
-	payment_mask = all_data['type'] == 'payment'
-	assert payment == all_data[payment_mask]['amount'].sum()
+	assert payment == all_data['paid_amount'].sum()
 
 	all_data.to_csv(f'{current_path}/output/all_samples.csv')
