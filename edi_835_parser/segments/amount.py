c0055ca934ed805562f49ded72a9d6cf0d926202
Keiron Stoddart <keiron.stoddart@gmail.com>
2021-08-17 16:04:38 -0400

    generalized the paid_amount attribute in the amount segment to amount since the amount described in the amount segment doesn't necessarily describe the amount that was paid

--- edi_835_parser/segments/amount.py
+++ edi_835_parser/segments/amount.py
@@ -9,7 +9,7 @@ class Amount:
 
 	identifier = Identifier()
 	qualifier = AmountQualifier()
-	paid_amount = Dollars()
+	amount = Dollars()
 
 	def __init__(self, segment: str):
 		self.segment = segment
@@ -17,7 +17,7 @@ class Amount:
 
 		self.identifier = segment[0]
 		self.qualifier = segment[1]
-		self.paid_amount = segment[2]
+		self.amount = segment[2]
 
 	def __repr__(self):
 		return '\n'.join(str(item) for item in self.__dict__.items())
