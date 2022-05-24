78f43b936d0b17a35e9a6f2957c386f58abeab31
Christopher Pickering <christopher.pickering@keemail.me>
2022-05-24 14:35:36 -0500

    added supported for uhc ^ delimiter, updated tests

--- edi_835_parser/elements/utilities.py
+++ edi_835_parser/elements/utilities.py
@@ -1,14 +1,5 @@
 from typing import List
-
+import re
 def split_element(segment: str) -> List[str]:
 	"""different payers use different characters to delineate sub-elements"""
-	arrow = '^'
-	colon = ':'
-
-	arrow_segment_count = len(segment.split(arrow))
-	colon_segment_count = len(segment.split(colon))
-
-	if arrow_segment_count > colon_segment_count:
-		return segment.split(arrow)
-	else:
-		return segment.split(colon)
+	return re.split(r"\^|:|>", segment)
--- tests/test_edi_835_parser.py
+++ tests/test_edi_835_parser.py
@@ -3,25 +3,30 @@ from tests.conftest import current_path
 def test_claim_count(
 		blue_cross_nc_sample,
 		emedny_sample,
+		united_healthcare_legacy_sample,
 		all_samples,
 ):
 	assert blue_cross_nc_sample.count_claims() == 1
+	assert united_healthcare_legacy_sample.count_claims() == 2
 	assert emedny_sample.count_claims() == 3
-	assert all_samples.count_claims() == 4
+	assert all_samples.count_claims() == 6
 
 def test_patient_count(
 		blue_cross_nc_sample,
 		emedny_sample,
+		united_healthcare_legacy_sample,
 		all_samples,
 
 ):
 	assert blue_cross_nc_sample.count_patients() == 1
+	assert united_healthcare_legacy_sample.count_patients() == 2
 	assert emedny_sample.count_patients() == 3
-	assert all_samples.count_patients() == 4
+	assert all_samples.count_patients() == 6
 
 def test_to_dataframe(
 		blue_cross_nc_sample,
 		emedny_sample,
+		united_healthcare_legacy_sample,
 		all_samples,
 ):
 	payment = blue_cross_nc_sample.sum_payments()
@@ -38,9 +43,16 @@ def test_to_dataframe(
 
 	emedny_data.to_csv(f'{current_path}/output/emedny_sample.csv')
 
+	payment = united_healthcare_legacy_sample.sum_payments()
+	united_healthcare_legacy_ = united_healthcare_legacy_sample.to_dataframe()
+
+	assert payment == united_healthcare_legacy_['paid_amount'].sum()
+
+	united_healthcare_legacy_.to_csv(f'{current_path}/output/united_healthcare_legacy_sample.csv')
+
 	payment = all_samples.sum_payments()
 	all_data = all_samples.to_dataframe()
 
-	assert payment == all_data['paid_amount'].sum()
+	assert payment == all_data['paid_amount'].sum().round(2)
 
 	all_data.to_csv(f'{current_path}/output/all_samples.csv')
