3824868c6c2fb6e3949abb72fffdf0be52e0e584
Christopher Pickering <cpickering@rhc.net>
2022-07-07 15:01:46 -0500

    added < delimiter for aetna better health

--- edi_835_parser/elements/utilities.py
+++ edi_835_parser/elements/utilities.py
@@ -2,7 +2,7 @@ from typing import List
 
 def split_element(segment: str) -> List[str]:
     """different payers use different characters to delineate sub-elements"""
-    delim_dict = {x:0 for x in ['^',':',">"]}
+    delim_dict = {x:0 for x in ['^',':','>', '<']}
 
     for delim in delim_dict:
         delim_dict[delim] = segment.count(delim)
